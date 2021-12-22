8e6bfd5084091bda6a8c9b742aae76ad1d1a3fd8
ITHwang <intaek428@gmail.com>
2021-12-22 09:43:07 +0900

    @Query로 메서드 정의하기

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
@@ -11,6 +12,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsername(@Param("username") String username);
 
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 
 }
 
